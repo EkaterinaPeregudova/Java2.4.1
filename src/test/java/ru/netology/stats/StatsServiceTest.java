114d800338e29a6018389479ab57f369fccfaeb8
katerina-kis <katerina-kis@yandex.ru>
2021-02-14 23:41:15 +0300

    сделала автотесты не только рабочими, но и красивыми

--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -3,67 +3,41 @@ package ru.netology.stats;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StatsServiceTest {
+    StatsService service = new StatsService();
+    long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
 
     @org.junit.jupiter.api.Test
     void sumArray() {
-        StatsService service = new StatsService();
-        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        long expected = 180;
-
-        long actual = service.sumArray(sales);
-
-        assertEquals(expected, actual);
+        assertEquals(180, service.sumArray(sales));
     }
 
     @org.junit.jupiter.api.Test
     void arithmeticMean() {
-        StatsService service = new StatsService();
-        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        double expected = 15;
-
-        double actual = service.arithmeticMean(sales);
-
-        assertEquals(expected, actual);
+        assertEquals(15, service.arithmeticMean(sales));
     }
+
     @org.junit.jupiter.api.Test
     void montOfMaxSale() {
-        StatsService service = new StatsService();
-        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        long expected = 8;
-
-        long actual = service.montOfMaxSale(sales);
-
-        assertEquals(expected, actual);
+        assertEquals(8, service.montOfMaxSale(sales));
     }
 
     @org.junit.jupiter.api.Test
     void montOfMinSale() {
-        StatsService service = new StatsService();
-        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        long expected = 9;
-
-        long actual = service.montOfMinSale(sales);
-
-        assertEquals(expected, actual);
+        assertEquals(9, service.montOfMinSale(sales));
     }
     @org.junit.jupiter.api.Test
     void sumMontMinMean() {
-        StatsService service = new StatsService();
-        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        long expected = 5;
-
-        long actual = service.sumMontMinMean(sales);
-
-        assertEquals(expected, actual);
+        assertEquals(5, service.sumMontMinMean(sales));
     }
+
     @org.junit.jupiter.api.Test
     void sumMontMaxMean() {
-        StatsService service = new StatsService();
-        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        long expected = 5;
+//        StatsService service = new StatsService();
+//        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+//        long expected = 5;
+//
+//        long actual = service.sumMontMaxMean(sales);
 
-        long actual = service.sumMontMaxMean(sales);
-
-        assertEquals(expected, actual);
+        assertEquals(5, service.sumMontMaxMean(sales));
     }
 }
